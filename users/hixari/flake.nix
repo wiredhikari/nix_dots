f6850d8b9ea70bb19ea73975093d4509abc54938
Atharva <81508805+wiredhikari@users.noreply.github.com>
2022-05-12 21:43:20 +0530

    Create flake.nix

---
+++ users/hixari/flake.nix
@@ -0,0 +1,19 @@
+{inputs = {
+nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";	
+nix.url = "github:nixos/nix";
+nixpkgs-f2k.inputs.nixos-unstable.follows = "nixpkgs";
+};
+
+ outputs = inputs@{ self, nixpkgs,nixpkgs-f2k , ... }:
+  {
+   nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+     modules = [
+        { nixpkgs.overlays = [ nixpkgs-f2k.overlay ]; }
+        ./configuration.nix
+      ];
+
+    specialArgs = { inherit attrs inputs; };
+    };
+  };
+}
